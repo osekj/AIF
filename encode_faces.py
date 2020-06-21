2496b2636f0c230c88602b681d497feb3466b9dd
osekj <jakubjnosek@gmail.com>
2020-06-21 19:28:49 +0200

    Remove unnecessary code; Add comments; Specify regions

--- encode_faces.py
+++ encode_faces.py
@@ -5,23 +5,24 @@ import paths
 import cv2
 import face_recognition
 
-
+# region Initializing command argument parser
 argsParser = argparse.ArgumentParser()
 argsParser.add_argument("-i", "--dataset", required=True,
                         help="path to input directory of faces + images")
 argsParser.add_argument("-e", "--encodings", required=True,
                         help="path to serialized db of facial encodings")
 args = vars(argsParser.parse_args())
+# endregion
 
-detectionMethod = "cnn"
-
+# region Prepare images and data
 print("[INFO] quantifying faces...")
 imagePaths = list(paths.list_images(args["dataset"]))
 
 knownEncodings = []
 knownNames = []
+#endregion
 
-# loop over the images
+# region Encoding
 for (i, imagePath) in enumerate(imagePaths):
     print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
     name = imagePath.split(os.path.sep)[-2]
@@ -29,16 +30,23 @@ for (i, imagePath) in enumerate(imagePaths):
     image = cv2.imread(imagePath)
     rgbImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    boxes = face_recognition.face_locations(rgbImage, model = detectionMethod)
+    boxes = face_recognition.face_locations(rgbImage)
     encodings = face_recognition.face_encodings(rgbImage, boxes)
 
     for encoding in encodings:
         knownEncodings.append(encoding)
         knownNames.append(name)
+# endregion
 
-# to disk
+# region Saving encodings to disk
+
+# It's better to save encodings as it takes some time to
+# obtain them and we don't want to wait every time when
+# there is a need to recognize a face
+
 print("[INFO] serializing encodings...")
 data = {"encodings": knownEncodings, "names": knownNames}
 f = open(args["encodings"], "wb")
 f.write(pickle.dumps(data))
 f.close()
+# endregion
--- recognize_faces.py
+++ recognize_faces.py
@@ -3,15 +3,16 @@ import pickle
 import argparse
 import cv2
 
+# region Initializing command argument parser
 argsParser = argparse.ArgumentParser()
 argsParser.add_argument("-e", "--encodings", required=True,
                         help="path to input directory of faces + images")
 argsParser.add_argument("-i", "--image", required=True,
                         help="path to serialized db of facial encodings")
 args = vars(argsParser.parse_args())
+#endregion
 
-detectionMethod = "cnn"
-
+# region Prepare image and data
 print("[INFO] loading encodings...")
 data = pickle.loads(open(args["encodings"], "rb").read())
 
@@ -19,14 +20,19 @@ image = cv2.imread(args["image"])
 rgbImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 print("[INFO] recognizing faces...")
-boxes = face_recognition.face_locations(rgbImage, model=detectionMethod)
+boxes = face_recognition.face_locations(rgbImage)
 encodings = face_recognition.face_encodings(rgbImage, boxes)
 
 names = []
+# endregion
 
+# region Finding best fit
+# Loop over the encodings to find the best fit to a given face
 for encoding in encodings:
     matches = face_recognition.compare_faces(data["encodings"], encoding)
-    name = "Unkown"
+
+    # As at the beginning we don't know whose face it is we set it to "Unknown"
+    name = "Unknown"
 
     if True in matches:
         matchedIndexes = [i for (i, b) in enumerate(matches) if b]
@@ -36,12 +42,14 @@ for encoding in encodings:
             name = data["names"][i]
             counts[name] = counts.get(name, 0) + 1
 
+            # Getting the name with the highest number of "votes"
             name = max(counts, key=counts.get)
 
         names.append(name)
+# endregion
 
-
-# Showing a rectangle with a given name around the detected face
+# region Displaying an image
+# Showing an image with a rectangle around the face with a given written above
 for ((top, right, bottom, left), name) in zip(boxes, names):
     cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
     y = top - 15 if top - 15 > 15 else top + 15
@@ -50,3 +58,4 @@ for ((top, right, bottom, left), name) in zip(boxes, names):
 
 cv2.imshow("Image", image)
 cv2.waitKey(0)
+# endregion
